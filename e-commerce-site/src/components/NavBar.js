2a56fab46ed49b1b4b67f1b62286ac9b03020f00
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-02-05 00:22:43 +0530

    NavBar Component in progress

--- e-commerce-site/src/components/NavBar.js
+++ e-commerce-site/src/components/NavBar.js
@@ -16,12 +16,21 @@ class NavBar extends Component {
                     </div>
 
                     <div className = "row-1-siteLogo">
-                        <h1>Octavia</h1>
+                        <h1>OCTAVIA</h1>
                     </div>
 
                     <div className = "row-1-utilities">
                         <a href = "/"><img src = "" alt = "Cart Icon" /></a>
                         <a href = "/"><img src = "" alt = "Search Icon" /></a>
+                    </div>
+                </div>
+
+                <div className = "nav-row-2">
+                    <div className = "row-2-contents">
+                        <a href = "/">Home</a>
+                        <a href = "/">Men</a>
+                        <a href = "/">Women</a>
+                        <a href = "/">About</a>
                     </div>
                 </div>
             </div>
